2833f8a03a04cb7e88d417063495eea035a47985
Andy Gikling <asgikling@gmail.com>
2013-09-09 18:31:10 -0500

    .Net - Added EyeModel and EyeViewModel helper classes.

---
+++ Source Code/BBotDotNET/BBot.Eye/EyeModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace BBot
+{
+    public class EyeModel : IRobotWidget
+    {
+        IRobotConnection connection;
+        int panPosition, tiltPosition;
+
+        const string op_EyeRemotePanTiltEnable = "E00";
+        const string op_EyeSoftwarePanTiltEnable = "E01";
+        const string op_EyeSoftwareSetPanTiltPosition = "E02";
+        const string op_Termination = "\r";
+
+        public EyeModel(IRobotConnection Connection)
+        {
+            RouteMessages(Connection);
+
+            PanPosition = 100;
+            TiltPosition = 100;
+        }
+
+        #region Properties
+
+        public int PanPosition
+        {
+            get
+            {
+                return panPosition;
+            }
+            set
+            {
+                panPosition = value;
+                SendPanTiltPosition(panPosition, tiltPosition);
+            }
+        }
+
+        public int TiltPosition
+        {
+            get
+            { 
+                return tiltPosition;
+            }
+            set
+            {
+                tiltPosition = value;
+                SendPanTiltPosition(panPosition, tiltPosition);
+            }
+        }
+
+        #endregion
+
+        #region IRobotWidget Members
+
+        public void SendMessage(string Message)
+        {
+            connection.SendMessage(Message); 
+        }
+
+        public void ReceiveMessage(string Message)
+        {
+        }
+
+        public void RouteMessages(IRobotConnection Connection)
+        {
+            connection = Connection;
+        }
+
+        public void InFocus(bool IsFocused)
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        void SendPanTiltModeSelect(bool SoftwarePanTiltMode)
+        {
+            string msg = "";
+            if (SoftwarePanTiltMode)
+            {
+                msg += op_EyeSoftwarePanTiltEnable;
+            }
+            else
+            {
+                msg += op_EyeRemotePanTiltEnable;
+            }
+            msg += " " + op_Termination;
+            SendMessage(msg);
+        }
+
+        void SendPanTiltPosition(int PanPosition, int TiltPosition)
+        {
+            string msg = "";
+            msg += op_EyeSoftwareSetPanTiltPosition;
+            msg += " " + PanPosition.ToString() + " " + TiltPosition.ToString() + op_Termination;
+            SendMessage(msg);
+        }
+        
+        #endregion
+
+    }
+}
---
+++ Source Code/BBotDotNET/BBot.Eye/EyeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBot
+{
+    public class EyeViewModel
+    {
+        EyeModel model;
+
+        public EyeViewModel(IRobotConnection Connection)
+        {
+            model = new EyeModel(Connection);
+        }
+
+        public EyeModel EyeModel
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+    }
+}
